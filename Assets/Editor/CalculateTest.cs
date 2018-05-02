f8b26c6403005cd6534813dd21088a1508d36634
as3mbus <asembus@gmail.com>
2018-05-02 13:48:28 +0700

    add simple test

---
+++ Assets/Editor/CalculateTest.cs
@@ -0,0 +1,35 @@
+﻿using NUnit.Framework;
+using UnityEngine;
+
+namespace as3mbus.Open_Fuzzy_Scenario.Editor.Test
+{
+    [TestFixture]
+    public class Test_Fuzzy_Value 
+    {
+      [Test]
+      public void testIsExist()
+      {
+        var test = new TestObj();
+        test.craft();
+        Assert.AreEqual(true,test.exist);
+      } 
+      [Test]
+      public void testCalculate()
+      {
+        var test = new TestObj();
+        test.craft();
+        Assert.AreEqual(true,test.exist);
+      } 
+      public class TestObj
+      {
+          public bool exist=false;
+          public TestObj()
+          {
+          }
+          public void craft()
+          {
+              this.exist=true;
+          }
+      }
+    }
+}
