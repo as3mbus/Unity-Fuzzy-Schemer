e64016866c431d73ef68dcf470fec0b859bd8170
as3mbus <asembus@gmail.com>
2018-05-02 13:49:27 +0700

    add linguistic variable class

---
+++ Assets/Fuzzy_Value.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace as3mbus.Open_Fuzzy_Scenario.Scripts.Object
+{
+    public class linguistic_Variable 
+    {
+        public string linguistic_Name;
+        Dictionary<string,float> linguistics_Value =
+            new Dictionary<string, float>();
+        Dictionary<string,string> linguistics_membership_function = 
+            new Dictionary<string, string>();
+        public void check_method(){
+            
+        }
+        
+    }
+}
