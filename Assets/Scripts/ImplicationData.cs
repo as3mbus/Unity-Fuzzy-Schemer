ab5f1a7ae5373b382269840b7c2ca7e77352351f
as3mbus <asembus@gmail.com>
2018-08-24 13:43:16 +0700

    Add Implication Data class to store implication graph results

---
+++ Assets/Scripts/ImplicationData.cs
@@ -0,0 +1,13 @@
+using System.Collections.Generic;
+namespace as3mbus.OpenFuzzyScenario.Scripts.Objects
+{
+    public class ImplicationData
+    {
+        public List<double> data = new List<double>();
+        public double spacing;
+        public List<double> chokePoint = new List<double>();
+        public double maximum;
+        public List<double> MaxAxis = new List<double>();
+        public double centerPoint;
+    }
+}
